12f80d40540794dd90554a2b7ecd2e2505c80fdd
Chanhee Park <parkchanhee@gmail.com>
2022-07-14 22:49:32 -0500

    Python module

---
+++ src/python/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+#
+# Copyright 2022, Micah Thornton and Chanhee Park <parkchanhee@gmail.com>
+#
+# This file is part of GEMMULEM
+#
+# GEMMULEM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GEMMULEM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GEMMULEM.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from distutils.core import setup, Extension
+
+module1 = Extension('gemmulem',
+#                    define_macros = [('DEBUG', '1')],
+                    include_dirs=['../lib'],
+#                    libraries=['stdc++'],
+                    extra_objects = ['../../build/src/lib/libem.a'],
+                    sources = ['pygemmulem.c'])
+
+setup(name = 'gemmulem',
+        version = '1.0',
+        ext_modules = [module1])
